0ce36ebab606fb338e31993909d0aa3405f654ae
rajwindersxxx <rajwindersxxx@gmail.com>
2025-08-01 00:51:46 +0530

    add lazy loading

--- src/App.tsx
+++ src/App.tsx
@@ -3,14 +3,17 @@ import "primeicons/primeicons.css";
 
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
-import Table from "./components/Table";
+import { lazy, Suspense } from "react";
+const Table = lazy(() => import("./components/Table"));
 
 const queryclient = new QueryClient();
 function App() {
   return (
     <>
       <QueryClientProvider client={queryclient}>
-        <Table />
+        <Suspense fallback={<h2 className="text-2xl">Loading...</h2>}>
+          <Table />
+        </Suspense>
         <ReactQueryDevtools />
       </QueryClientProvider>
     </>
